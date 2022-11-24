cf4d9f45935792e31c538f60708bd79463cd7934
Ahmad Shah <101276981+SyedMuhammadAhmadShah123@users.noreply.github.com>
2022-11-24 21:26:34 +0500

    commit

---
+++ 10) Objects/scripts.js
@@ -0,0 +1,86 @@
+// ⭐ objects ⭐
+/*
+An object is made of key value pairs. Keys can be strings 
+(which don't require quotes), array with a string, or symbols. Values can be arrays,
+objects, numbers etc
+
+➡  syntax
+const objectName = {
+  member1Name: member1Value,
+  member2Name: member2Value,
+  member3Name: member3Value
+};
+*/
+// 1) Object Literal
+	const objLit = {
+    	name: "Ahmad",
+      	age: 20
+    }
+console.log(objLit.name)    //Ahmad
+console.log(objLit.age)     //20
+
+// 2) New key word
+class ObjectLayout {
+	constructor() {
+    	this.firstName = "Ahmad"; //property
+    }
+  
+  	sayHi() { // Method
+    	return `Hello from ${this.firstName}`;
+    }
+}
+const object = new ObjectLayout();
+console.log(object.firstName) // Ahmad;
+console.log(object.sayHi()) // "Hello from Larry"
+
+
+//🌞Example
+let testSymbol = Symbol('item number 3')
+const obj = {
+	item1: 1,
+    "item number 2": 2,
+    testSymbol: 3,
+  	['a']: 4
+}
+
+// Another way of creating an object:
+const obj1 = new Object();
+obj1.name = 'John'
+
+// 🌟Accessing value
+// To access values, you can use 1️⃣ dot notation or 2️⃣ bracket notation. 
+// 1️⃣ dot notation 
+console.log(obj.item1)
+// 2️⃣ bracket notation. 
+console.log( obj['item number 2'])
+// ◾ Both do the same thing, bracket notion is useful for multi space keys,
+// ◾ keys with dashes, or accessing values using variables
+
+ let b = 'item1'
+  console.log(obj[b])   //1
+  // The following would NOT work and would return undefined:
+  console.log(obj.b)    //undefined
+
+
+
+// Short hand key assignment:
+const name1 = 'John'
+const obj2 = {
+    // this is short hand that automatically sets the key to be 'name',
+    // and it's value to be 'John'
+	name1, 
+}
+
+
+// ⚡ Methods in objects
+const obj4 = {
+  	name: 'Mike',
+  	number: 4421,
+	sayHi: () => 'hi',
+  	sayBye() {
+    	return 'bye'
+    }
+}
+
+console.log(obj4.sayHi())   //hi
+console.log(obj4.sayBye())  //bye
--- 10) Objects/tempCodeRunnerFile.js
+++ 10) Objects/tempCodeRunnerFile.js
@@ -1,6 +1,1 @@
-for (let i in myObj.cars) {
-  x += "<h2>" + myObj.cars[i].name + "</h2>";
-  for (let j in myObj.cars[i].models) {
-    x += myObj.cars[i].models[j] + "<br>";
-  }
-}
+"Ahmad", "25"
